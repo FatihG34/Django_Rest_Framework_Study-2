ce1a17b18e2fa6b0013a336c78299986932849f0
FatihG34 <fthgnydn34@gmail.com>
2022-09-28 10:16:33 +0300

    serializer added

--- books/serializers.py
+++ books/serializers.py
@@ -3,12 +3,15 @@ from rest_framework import serializers
 from .models import Book, Comment
 
 
-class BooksSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Book
-        fields = "__all__"
-
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
+        fields = "__all__"
+
+
+class BooksSerializer(serializers.ModelSerializer):
+    comments = CommentSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Book
         fields = "__all__"
