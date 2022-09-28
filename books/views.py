6c91a1e5f4359256a80b9c58a9f50c97096346b2
FatihG34 <fthgnydn34@gmail.com>
2022-09-28 11:43:37 +0300

    detail added

--- books/views.py
+++ books/views.py
@@ -1,19 +1,28 @@
 from ast import arg
 from rest_framework.generics import GenericAPIView
+from rest_framework import generics
 from rest_framework.mixins import ListModelMixin, CreateModelMixin
 from .serializers import BooksSerializer
 from .models import Book
 # Create your views here.
 
 
-class BookListCreateAPIView(ListModelMixin, CreateModelMixin, GenericAPIView):
+class BookListCreateAPIView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BooksSerializer
 
-    # List all Books
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
+class BookDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BooksSerializer
 
-    # Create a new Book
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
+# class BookListCreateAPIView(ListModelMixin, CreateModelMixin, GenericAPIView):
+#     queryset = Book.objects.all()
+#     serializer_class = BooksSerializer
+
+#     # List all Books
+#     def get(self, request, *args, **kwargs):
+#         return self.list(request, *args, **kwargs)
+
+#     # Create a new Book
+#     def post(self, request, *args, **kwargs):
+#         return self.create(request, *args, **kwargs)
